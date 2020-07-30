8bf22e5212244c738bfc5d6ab0a7ea305d70089f
ThilakaHome <v.thilaka@gmail.com>
2020-07-30 22:36:40 +1000

    Get, Post, Put, Delete requests

---
+++ course-api/src/main/java/com/thilaka/springbootstarter/topic/TopicService.java
@@ -0,0 +1,41 @@
+package com.thilaka.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("java", "Core Java", "Core Java Description"),
+			new Topic("javascript", "JavaScript","JavaScript Description")
+			));
+			
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	public Topic getTopic(String id){
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	
+	}
+
+	public void updateTopic(String id, Topic topic) {	
+		topics.replaceAll(t -> t.getId().equals(id)? topic : t);
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+
+}
